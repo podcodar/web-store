e510b7ffcc705adaf335dff824d651a043c3ceb3
Thiago Pereira <thiagopereiramail@gmail.com>
2021-12-27 18:24:51 +0000

    fix encapsulate the body of the shopping cart item

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,9 +1,9 @@
-import { Box, Button, HStack, Image, Text } from '@chakra-ui/react';
+import { Box, Button, Text } from '@chakra-ui/react';
 
+import CartItem from '@packages/components/CartItem';
 import Footer from '@packages/components/Footer';
 import ICart from '@packages/entities/ICart';
 import useStorage from '@packages/hooks/useStorage';
-import { currencyFormat } from '@packages/utils/functions';
 
 export default function Cart() {
   const [cart, setCart] = useStorage<ICart>('cart');
@@ -21,46 +21,11 @@ export default function Cart() {
           <Box minH="70vh">
             <Box>
               {cart.items.map((item) => (
-                <Box key={item.product.id} d="flex" marginBottom="20px">
-                  <Box marginRight="15px">
-                    <Image
-                      src={item.product.img}
-                      alt={item.product.title}
-                      w="300px"
-                    />
-                  </Box>
-                  <Box>
-                    <Text fontWeight="bold" fontSize="20px">
-                      {item.product.title}
-                    </Text>
-                    <Text textAlign="justify" marginBottom="5px">
-                      {item.product.fullDescription}
-                    </Text>
-                    <HStack spacing="30px">
-                      <HStack>
-                        <Text fontWeight="bold">Preço:</Text>
-                        <Text>{currencyFormat(item.product.price)}</Text>
-                      </HStack>
-                      <HStack>
-                        <Text fontWeight="bold">Quantidade:</Text>
-                        <Text>{item.quantity}</Text>
-                      </HStack>
-                      <HStack>
-                        <Text fontWeight="bold">Sub Total:</Text>
-                        <Text>
-                          {currencyFormat(item.product.price * item.quantity)}
-                        </Text>
-                      </HStack>
-                      <Button
-                        bgColor="gray.200"
-                        _hover={{ bgColor: 'gray.300' }}
-                        onClick={() => removeFromCart(item.product.id)}
-                      >
-                        Remover
-                      </Button>
-                    </HStack>
-                  </Box>
-                </Box>
+                <CartItem
+                  key={item.product.id}
+                  item={item}
+                  onRemove={() => removeFromCart(item.product.id)}
+                />
               ))}
             </Box>
 
