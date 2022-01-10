117e0e5727b036484487213f74afbaf576a72067
Thiago Pereira <thiagopereiramail@gmail.com>
2022-01-10 16:22:32 +0000

    make CartItem and CartItemList components responsive

--- packages/components/CartItem.tsx
+++ packages/components/CartItem.tsx
@@ -1,4 +1,4 @@
-import { Image, Text, HStack, Tr, Td, IconButton } from '@chakra-ui/react';
+import { Image, Text, IconButton, Stack, Box, chakra } from '@chakra-ui/react';
 import { FaTrash } from 'react-icons/fa';
 
 import ICartItem from '@packages/entities/ICartItem';
@@ -9,28 +9,66 @@ interface Props {
   onRemove: (item: ICartItem) => void;
 }
 
+const Title = chakra(Text, {
+  baseStyle: {
+    textAlign: 'center',
+    textTransform: 'uppercase',
+    fontSize: '14px',
+    fontWeight: 'bold',
+    color: 'gray.700',
+    margin: '8px 0px',
+    display: { base: 'block', lg: 'none' },
+  },
+});
+
+const TCell = chakra(Box, {
+  baseStyle: {
+    minW: '7em',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: '5px 0px',
+  },
+});
+
 export default function CartItem({ item, onRemove }: Props) {
   return (
-    <Tr key={item.product.id}>
-      <Td d="flex" alignItems="center">
-        <HStack spacing="20px">
-          <Image src={item.product.img} alt={item.product.title} w="100px" />
-          <Text>{item.product.description}</Text>
-        </HStack>
-      </Td>
-      <Td>{currencyFormat(item.product.price)}</Td>
-      <Td>{item.quantity}</Td>
-      <Td>{currencyFormat(item.product.price * item.quantity)}</Td>
-      <Td>
+    <Stack m="8px 0px" direction={{ base: 'column', lg: 'row' }}>
+      <TCell>
+        <Title>Produto</Title>
+        <Image
+          src={item.product.img}
+          alt={item.product.title}
+          w={{ base: '50%', lg: '6em' }}
+        />
+      </TCell>
+      <TCell w="full">
+        <Title>Descrição</Title>
+        <Text textAlign="center">{item.product.description}</Text>
+      </TCell>
+      <TCell>
+        <Title>Preço</Title>
+        {currencyFormat(item.product.price)}
+      </TCell>
+      <TCell>
+        <Title>Quantidade</Title>
+        {item.quantity}
+      </TCell>
+      <TCell>
+        <Title>Subtotal</Title>
+        {currencyFormat(item.product.price * item.quantity)}
+      </TCell>
+      <TCell>
         <IconButton
           aria-label="delete button"
           color="red.400"
           icon={<FaTrash />}
-          bgColor="gray.200"
+          bgColor="gray.300"
           _hover={{ bgColor: 'red.200' }}
           onClick={() => onRemove(item)}
         />
-      </Td>
-    </Tr>
+      </TCell>
+    </Stack>
   );
 }
--- packages/components/CartItemsList.tsx
+++ packages/components/CartItemsList.tsx
@@ -1,14 +1,4 @@
-import {
-  Button,
-  Table,
-  Tbody,
-  Td,
-  Text,
-  Tfoot,
-  Th,
-  Thead,
-  Tr,
-} from '@chakra-ui/react';
+import { Box, Button, chakra, Stack, Text } from '@chakra-ui/react';
 import { FaArrowLeft } from 'react-icons/fa';
 import { useRouter } from 'next/router';
 
@@ -21,46 +11,74 @@ interface Props {
   onRemove: (item: ICartItem) => void;
 }
 
+const Title = chakra(Text, {
+  baseStyle: {
+    textAlign: 'center',
+    textTransform: 'uppercase',
+    fontSize: '14px',
+    fontWeight: 'bold',
+    color: 'gray.700',
+  },
+});
+
+const TCell = chakra(Box, {
+  baseStyle: {
+    minW: '7em',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+});
+
 export default function CartItemsList({ items = [], onRemove }: Props) {
   const router = useRouter();
 
   return (
-    <Table variant="simple">
-      <Thead>
-        <Tr>
-          <Th>Produto</Th>
-          <Th isNumeric>Preço</Th>
-          <Th isNumeric>Quantidade</Th>
-          <Th colSpan={2}>SubTotal</Th>
-        </Tr>
-      </Thead>
-      <Tbody>
-        {items.length ? (
-          items.map((item) => (
-            <CartItem key={item.product.id} item={item} onRemove={onRemove} />
-          ))
-        ) : (
-          <Tr>
-            <Td colSpan={5}>
-              <Text textAlign="center">Não há Itens no carrinho</Text>
-            </Td>
-          </Tr>
-        )}
-      </Tbody>
-      <Tfoot>
-        <Tr>
-          <Td colSpan={5}>
-            <Button
-              leftIcon={<FaArrowLeft />}
-              bgColor="gray.200"
-              _hover={{ bgColor: 'gray.300' }}
-              onClick={() => router.push('/')}
-            >
-              Continuar Comprando
-            </Button>
-          </Td>
-        </Tr>
-      </Tfoot>
-    </Table>
+    <Box w="full">
+      <Stack display={{ base: 'none', lg: 'flex' }} direction="row">
+        <TCell>
+          <Title>Produto</Title>
+        </TCell>
+        <TCell w="full">
+          <Title>Descrição</Title>
+        </TCell>
+        <TCell>
+          <Title>Preço</Title>
+        </TCell>
+        <TCell>
+          <Title>Quantidade</Title>
+        </TCell>
+        <TCell>
+          <Title>Subtotal</Title>
+        </TCell>
+        <TCell> </TCell>
+      </Stack>
+
+      {items.length ? (
+        items.map((item) => (
+          <CartItem key={item.product.id} item={item} onRemove={onRemove} />
+        ))
+      ) : (
+        <Text m="5px" textAlign="center">
+          Não há Itens no carrinho
+        </Text>
+      )}
+
+      <Box
+        d="flex"
+        flexDirection="column"
+        alignItems={{ base: 'center', lg: 'flex-start' }}
+      >
+        <Button
+          margin="40px 0px"
+          leftIcon={<FaArrowLeft />}
+          bgColor="gray.300"
+          _hover={{ bgColor: 'gray.400' }}
+          onClick={() => router.push('/')}
+        >
+          Continuar Comprando
+        </Button>
+      </Box>
+    </Box>
   );
 }
