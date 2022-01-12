3e02cad69ba67dc1baee596dd6f16029e177aecc
Thiago Pereira <thiagopereiramail@gmail.com>
2022-01-12 10:53:02 +0000

    remove header of columns

--- packages/components/CartItemsList.tsx
+++ packages/components/CartItemsList.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, chakra, Stack, Text } from '@chakra-ui/react';
+import { Box, Button, Text } from '@chakra-ui/react';
 import { FaArrowLeft } from 'react-icons/fa';
 import { useRouter } from 'next/router';
 
@@ -11,49 +11,11 @@ interface Props {
   onRemove: (item: ICartItem) => void;
 }
 
-const Title = chakra(Text, {
-  baseStyle: {
-    textAlign: 'center',
-    textTransform: 'uppercase',
-    fontSize: '14px',
-    fontWeight: 'bold',
-    color: 'gray.700',
-  },
-});
-
-const TCell = chakra(Box, {
-  baseStyle: {
-    minW: '7em',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-});
-
 export default function CartItemsList({ items = [], onRemove }: Props) {
   const router = useRouter();
 
   return (
     <Box w="full">
-      <Stack display={{ base: 'none', lg: 'flex' }} direction="row">
-        <TCell>
-          <Title>Produto</Title>
-        </TCell>
-        <TCell w="full">
-          <Title>Descrição</Title>
-        </TCell>
-        <TCell>
-          <Title>Preço</Title>
-        </TCell>
-        <TCell>
-          <Title>Quantidade</Title>
-        </TCell>
-        <TCell>
-          <Title>Subtotal</Title>
-        </TCell>
-        <TCell> </TCell>
-      </Stack>
-
       {items.length ? (
         items.map((item) => (
           <CartItem key={item.product.id} item={item} onRemove={onRemove} />
