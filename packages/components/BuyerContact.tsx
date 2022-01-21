680f9fc9a7976b0cb5e9fd2c3bc423fc49872fe7
Thiago Pereira <thiagopereiramail@gmail.com>
2022-01-21 18:45:12 +0000

    Form validation with formik

--- packages/components/BuyerContact.tsx
+++ packages/components/BuyerContact.tsx
@@ -11,7 +11,9 @@ import {
   Icon,
   InputProps,
   FormLabelProps,
+  Button,
 } from '@chakra-ui/react';
+import { useFormik } from 'formik';
 
 const labelStyle: FormLabelProps = {
   fontSize: '14px',
@@ -29,46 +31,110 @@ const iconStyle = {
 };
 
 export default function BuyerContact() {
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      phone: '',
+    },
+    validate: (values) => {
+      const errors: any = {};
+
+      if (!values.name) {
+        errors.name = 'Nome é requerido.';
+      } else if (values.name.length < 5) {
+        errors.name = 'Nome dever conter no mínimo 5 caracteres.';
+      }
+
+      if (!values.email) {
+        errors.email = 'E-mail é requerido.';
+      } else if (
+        !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)
+      ) {
+        errors.email = 'E-mail inválido.';
+      }
+
+      return errors;
+    },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2));
+    },
+  });
+
   return (
     <Stack direction="column" spacing="1em" marginBottom="5em">
       <Text marginTop="1em" fontSize="25px" fontWeight="bold" color="black">
         Contato
       </Text>
 
-      <Stack direction="column" spacing="2em">
-        <FormControl isRequired>
-          <FormLabel {...labelStyle}>Nome:</FormLabel>
-          <InputGroup>
-            <InputLeftElement pointerEvents="none">
-              <Icon as={FaUser} {...iconStyle} />
-            </InputLeftElement>
-            <Input id="name" {...inputStyle} />
-          </InputGroup>
-          <FormErrorMessage>Nome é requerido.</FormErrorMessage>
-        </FormControl>
+      <form onSubmit={formik.handleSubmit}>
+        <Stack direction="column" spacing="2em">
+          <FormControl isInvalid={formik.errors.name != undefined} isRequired>
+            <FormLabel {...labelStyle} htmlFor="name">
+              Nome:
+            </FormLabel>
+            <InputGroup>
+              <InputLeftElement pointerEvents="none">
+                <Icon as={FaUser} {...iconStyle} />
+              </InputLeftElement>
+              <Input
+                {...inputStyle}
+                id="name"
+                name="name"
+                value={formik.values.name}
+                onChange={formik.handleChange}
+              />
+            </InputGroup>
+            <FormErrorMessage>{formik.errors.name}</FormErrorMessage>
+          </FormControl>
 
-        <FormControl isRequired>
-          <FormLabel {...labelStyle}>E-mail:</FormLabel>
-          <InputGroup>
-            <InputLeftElement pointerEvents="none">
-              <Icon as={FaEnvelope} {...iconStyle} />
-            </InputLeftElement>
-            <Input id="email" type="email" {...inputStyle} />
-          </InputGroup>
-          <FormErrorMessage>Email é requerido.</FormErrorMessage>
-        </FormControl>
+          <FormControl isInvalid={formik.errors.email != undefined} isRequired>
+            <FormLabel {...labelStyle} htmlFor="email">
+              E-mail:
+            </FormLabel>
+            <InputGroup>
+              <InputLeftElement pointerEvents="none">
+                <Icon as={FaEnvelope} {...iconStyle} />
+              </InputLeftElement>
+              <Input
+                {...inputStyle}
+                id="email"
+                name="email"
+                type="email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+              />
+            </InputGroup>
+            <FormErrorMessage>{formik.errors.email}</FormErrorMessage>
+          </FormControl>
 
-        <FormControl isRequired>
-          <FormLabel {...labelStyle}>Telefone:</FormLabel>
-          <InputGroup>
-            <InputLeftElement pointerEvents="none">
-              <Icon as={FaPhone} {...iconStyle} />
-            </InputLeftElement>
-            <Input id="phone" type="tel" {...inputStyle} />
-          </InputGroup>
-          <FormErrorMessage>Telefone é requerido.</FormErrorMessage>
-        </FormControl>
-      </Stack>
+          <FormControl isRequired>
+            <FormLabel {...labelStyle} htmlFor="phone">
+              Telefone:
+            </FormLabel>
+            <InputGroup>
+              <InputLeftElement pointerEvents="none">
+                <Icon as={FaPhone} {...iconStyle} />
+              </InputLeftElement>
+              <Input
+                {...inputStyle}
+                id="phone"
+                name="phone"
+                type="tel"
+                value={formik.values.phone}
+                onChange={formik.handleChange}
+              />
+            </InputGroup>
+            <FormErrorMessage>Telefone é requerido.</FormErrorMessage>
+          </FormControl>
+
+          <Stack direction="row">
+            <Button type="submit" bgColor="fifth.300">
+              Próximo
+            </Button>
+          </Stack>
+        </Stack>
+      </form>
     </Stack>
   );
 }
