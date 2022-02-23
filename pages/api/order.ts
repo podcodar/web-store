9cc9fd0ba255765874daffab5d0f43f65fa67fe6
Thiago Pereira <thiagopereiramail@gmail.com>
2022-02-23 17:28:33 +0000

    receive credentials for authentication

--- pages/api/order.ts
+++ pages/api/order.ts
@@ -1,8 +1,21 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default function OrderAPI(req: NextApiRequest, res: NextApiResponse) {
+const NOTION_KEY = process.env.NOTION_KEY;
+const NOTION_DB_ID = process.env.NOTION_DB_ID;
+
+export default async function OrderAPI(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
   try {
-    console.info('body: ', req.body);
+    // console.info('body: ', req.body);
+    if (!NOTION_KEY) {
+      throw new Error('Integration Token not Informed!');
+    }
+
+    if (!NOTION_DB_ID) {
+      throw new Error('Database ID not Informed!');
+    }
 
     res.status(200).json({ name: 'John Doe' });
   } catch (error) {
