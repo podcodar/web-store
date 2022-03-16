91443decb3245226f50b4ee053182ab7db27db42
Guilherme barbosa <guilhermebarbo17@hotmail.com>
2022-03-16 10:57:43 -0300

    change cart image

--- packages/config/site.ts
+++ packages/config/site.ts
@@ -10,7 +10,7 @@ export const images = {
   icon: '/images/favicon.svg',
   logo: '/images/just-llama.png',
   pixQRCode: '/images/pix-qrcode.png',
-  cart: '/images/cart.png',
+  cart: '/images/cart.svg',
   pixLogo: '/images/pix-logo.png',
 };
 
