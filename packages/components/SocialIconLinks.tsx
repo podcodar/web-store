829072597fe8c4d8bdfed6a844ab8171796ed270
Thiago Pereira <thiagopereiramail@gmail.com>
2022-03-18 18:01:59 -0300

    remove facebook and instagram icons from footer (#35)
    Fix: remove facebook and instagram icons from footer

--- packages/components/SocialIconLinks.tsx
+++ packages/components/SocialIconLinks.tsx
@@ -1,10 +1,5 @@
 import { Stack, Icon } from '@chakra-ui/react';
-import {
-  FaFacebook,
-  FaInstagramSquare,
-  FaLinkedin,
-  FaGithub,
-} from 'react-icons/fa';
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
 
 import Link from '@packages/components/Link';
 import { links } from '@packages/config/site';
@@ -17,12 +12,6 @@ export default function SocialIconLinks() {
       direction="row"
       fontSize="1.3rem"
     >
-      <Link href="/">
-        <Icon as={FaFacebook} color="white" />
-      </Link>
-      <Link href="/">
-        <Icon as={FaInstagramSquare} color="white" />
-      </Link>
       <Link href={links.linkedin}>
         <Icon as={FaLinkedin} color="white" />
       </Link>
