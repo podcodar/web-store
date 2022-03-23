3de683451f973d2880379a38535ab747e9cee9fd
Thiago Pereira <thiagopereiramail@gmail.com>
2022-03-23 17:30:59 +0000

    create the alert to notify the store is in development

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import { StyleProps, Text } from '@chakra-ui/react';
+
 import Footer from '@packages/components/Footer';
 import LaunchCountDown from '@packages/components/LaunchCountDown';
 import ProductGrid from '@packages/components/ProductGrid';
@@ -9,9 +11,27 @@ interface Props {
   products: IProduct[];
 }
 
+const textStyle1: StyleProps = {
+  color: 'red',
+  fontSize: '18px',
+  fontWeight: 'bold',
+  textAlign: 'center',
+};
+
+const textStyle2: StyleProps = {
+  color: 'red',
+  fontSize: '30px',
+  fontWeight: 'bold',
+  textAlign: 'center',
+  marginTop: '20px',
+};
+
 export default function Home({ products }: Props) {
   return (
     <>
+      <Text sx={textStyle2}>Atenção</Text>
+      <Text sx={textStyle1}>Todos os Produtos abaixo são para testes!</Text>
+      <Text sx={textStyle1}>Estamos em processo de desenvolvimento!</Text>
       <LaunchCountDown />
       <ProductGrid products={products} />
       <Footer />
