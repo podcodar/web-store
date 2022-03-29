7fbc288e1b2a32ab6cf6fdad36b7bc8dc986fd9f
Thiago Pereira <thiagopereiramail@gmail.com>
2022-03-29 18:32:04 +0000

    resize image and adjust layout

--- db.ts
+++ db.ts
@@ -4,29 +4,29 @@ export const products: IProduct[] = [
   {
     id: 1,
     title: 'Caneca PodCodar - Foguetinho',
-    img: '/images/products/caneca01-foguetinho01.png',
+    img: '/images/products/caneca01-foguetinho02.png',
     price: 35,
     discount: 14,
-    description: 'Caneca estampada com foguetinho da PodCodar',
+    description: 'Caneca PodCodar com estampa do Foguetinho',
     fullDescription:
       'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. ',
   },
   {
     id: 2,
     title: 'Caneca PodCodar - Céu Estrelado',
-    img: '/images/products/caneca03-ceu01.png',
-    price: 10,
-    discount: 50,
-    description: 'Caneca com estampa do céu e logo da PodCodar',
+    img: '/images/products/caneca02-ceu02.png',
+    price: 35,
+    discount: 14,
+    description: 'Caneca PodCodar com estampa do Céu Estrelado',
     fullDescription:
       'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. ',
   },
   {
     id: 3,
     title: 'Caneca PodCodar - Iema Rosa',
-    img: '/images/products/caneca05-iema-rosa01.png',
-    price: 10,
-    discount: 0,
+    img: '/images/products/caneca03-iema-rosa03.png',
+    price: 35,
+    discount: 14,
     description: 'Caneca PodCodar com estampa da Iema e fundo Rosa',
     fullDescription:
       'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. ',
@@ -34,9 +34,9 @@ export const products: IProduct[] = [
   {
     id: 4,
     title: 'Caneca PodCodar - Iema Marron',
-    img: '/images/products/caneca08-iema-marron01.png',
-    price: 10,
-    discount: 0,
+    img: '/images/products/caneca04-iema-marron02.png',
+    price: 35,
+    discount: 14,
     description: 'Caneca PodCodar com estampa da Iema e fundo Marron',
     fullDescription:
       'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. ',
@@ -44,9 +44,9 @@ export const products: IProduct[] = [
   {
     id: 5,
     title: 'Caneca PodCodar - Comunidade',
-    img: '/images/products/caneca11-comunidade02.png',
-    price: 10,
-    discount: 0,
+    img: '/images/products/caneca05-comunidade02.png',
+    price: 35,
+    discount: 14,
     description: 'Caneca PodCodar com estampa Comunidade',
     fullDescription:
       'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. ',
