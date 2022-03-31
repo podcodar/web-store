5e29e81c69c73c97d5674b33a72d755b94518eb8
Thiago Pereira <thiagopereiramail@gmail.com>
2022-03-31 10:37:53 +0000

    pull request reviews

--- db.ts
+++ db.ts
@@ -8,37 +8,24 @@ export const products: IProduct[] = [
     price: 35,
     discount: 14,
     description: 'Caneca PodCodar com estampa do Foguetinho',
-    fullDescription:
-      'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. ',
+    fullDescription: 'Caneca PodCodar com estampa do Foguetinho.',
   },
   {
     id: 2,
-    title: 'Caneca PodCodar - Céu Estrelado',
-    img: '/images/products/caneca02-ceu02.png',
+    title: 'Caneca PodCodar - Lhama',
+    img: '/images/products/caneca03-lhama-rosa01.png',
     price: 35,
     discount: 14,
-    description: 'Caneca PodCodar com estampa do Céu Estrelado',
-    fullDescription:
-      'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. ',
+    description: 'Caneca PodCodar com estampa da Lhama',
+    fullDescription: 'Caneca PodCodar com estampa da Lhama.',
   },
   {
     id: 3,
-    title: 'Caneca PodCodar - Lhama Rosa',
-    img: '/images/products/caneca03-lhama-rosa03.png',
-    price: 35,
-    discount: 14,
-    description: 'Caneca PodCodar com estampa da Lhama e fundo Rosa',
-    fullDescription:
-      'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. ',
-  },
-  {
-    id: 4,
     title: 'Caneca PodCodar - Comunidade',
-    img: '/images/products/caneca05-comunidade02.png',
+    img: '/images/products/caneca05-comunidade04.png',
     price: 35,
     discount: 14,
     description: 'Caneca PodCodar com estampa Comunidade',
-    fullDescription:
-      'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here, content here, making it look like readable English. ',
+    fullDescription: 'Caneca PodCodar com estampa Comunidade.',
   },
 ];
