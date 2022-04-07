c6228eb38771237a8fc7b721dbc5a9be790e32c5
Cassios Marques <cassios.kmm@gmail.com>
2022-04-07 20:20:02 -0300

    Anticipate launch datetime

--- packages/components/LaunchCountDown.tsx
+++ packages/components/LaunchCountDown.tsx
@@ -3,7 +3,7 @@ import { Box, Grid, GridItem, StyleProps, Text } from '@chakra-ui/react';
 
 import { useEffectOnce } from '@packages/utils/react';
 
-const RELEASE_DATE = '2022-04-07T21:00:00';
+const RELEASE_DATE = '2022-04-07T20:40:00';
 
 const boxStyle: StyleProps = {
   padding: '0.5em 0em',
